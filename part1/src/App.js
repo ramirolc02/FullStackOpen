b80466dce8db76b3e75ed712046fbf99806434c7
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-06-21 18:25:53 +0200

    part1 examples

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,30 @@
-const App = () => (
+const Hello = (props) => {
+  return (
   <div>
-    <p>Hello world</p>
+    <p>Hello{props.name}, you are {props.age} years old.</p>
   </div>
-)
+  )
+}
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/ramirolc02/FullStack-Projects">rama</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
 export default App
