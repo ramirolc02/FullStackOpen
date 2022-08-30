2cff9de1af45f8c53848d14080e439f039d3d2e4
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-08-30 16:57:40 +0200

    step8 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Persons from "./components/Persons"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
+import personService from "./services/persons"
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter,setNewFilter] = useState('')
-  const [personsCopy, setNewPersonsCopy] = useState([]);
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-        setNewPersonsCopy(response.data)
+   personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
-  console.log('render', persons.length, 'persons')
 
    const addPerson = (event) => {
     event.preventDefault()
@@ -32,8 +27,12 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
-      setNewPersonsCopy(persons.concat(personObject));
+      personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+      })
+
     }
     else{
         alert(`${newName} is already added to phonebook`)
@@ -52,10 +51,10 @@ const App = () => {
 
  const handleFilter = (event) => {
   // state change is asynchronous
-    const input = event.target.value;
-    setNewFilter(input)
-    setNewPersonsCopy(persons.filter(person => person.name.toLowerCase().includes(input)))
+    setNewFilter(event.target.value)
  }
+
+ const personsCopy = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div>
