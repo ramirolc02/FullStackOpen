787e08b9aab79b22f9c5e11d9897636921750347
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-11-01 15:18:32 +0100

    Changing port for backend connection

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
