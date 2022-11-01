e01dcc0ec92ced4c1b22100cd408904c4bbfadf9
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-11-01 15:21:13 +0100

    error in port solved

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,8 +3,3 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
