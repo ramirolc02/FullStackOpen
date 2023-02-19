a58930cfb266da2224ac7c6989df269ab8087349
Rama Lopez Cento <ramirolopezcento@icloud.com>
2023-02-19 22:56:55 +0100

    4.9 done (made beforeEach and helper )

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -4,11 +4,28 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json and have length as DB', async () => {
+    const allBlogs = await api
+         .get('/api/blogs')
+         .expect(200)
+         .expect('Content-Type', /application\/json/)
+
+         expect(allBlogs.body).toHaveLength(2)
+}, 100000) // error ms timeout
+
+test('unique identifier is named id', async () => {
+    const res = await api.get('/api/blogs')
+    res.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+    })
 })
 
 afterAll(async () => {
