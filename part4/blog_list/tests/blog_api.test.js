299363b4663af7aca2bc31bbf4c68f5de3d72832
Rama Lopez Cento <ramirolopezcento@icloud.com>
2023-02-19 23:10:39 +0100

    4.10 done (check length and title saved correctly)

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -28,6 +28,30 @@ test('unique identifier is named id', async () => {
     })
 })
 
+test('post creates new blog succesfully', async () => {
+
+    const newBlog = {
+        "title": "Atomic Habits",
+         "author": "James Clear ",
+         "url":"https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj018PohIL9AhUKKewKHTFaCigQFnoECCUQAQ&url=https%3A%2F%2Fjamesclear.com%2Fatomic-habits&usg=AOvVaw0o5PGECsACPPj94i7MJHzT",
+         "likes": 999
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+        const title = blogsAtEnd.map(blog => blog.title)
+        expect(title).toContain(
+            'Atomic Habits'
+        )
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
